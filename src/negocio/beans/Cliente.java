659227119cff0857c0f13b1ab7e84af74197fb31
SteffanoP <steffanoxpereira@gmail.com>
2021-06-16 20:10:46 -0300

    feat(beans): Construtor Cliente Clone

--- src/negocio/beans/Cliente.java
+++ src/negocio/beans/Cliente.java
@@ -4,12 +4,21 @@ import java.util.Objects;
 
 //TODO: Implementar a classe Cliente
 //TODO: Trabalhar com equals observando classe abstrata
-public class Cliente extends Pessoa {
+public class Cliente extends Pessoa implements Cloneable {
     //Atributos
     private int score;
 
     //Construtores
     public Cliente() {
+    }
+
+    public Cliente retornaClone() {
+        try {
+            return (Cliente) this.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     //Métodos
