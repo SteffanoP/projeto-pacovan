616f4cc769565da0ab1f017a2392432dd244d1a5
hagafedra <haga.fedra@gmail.com>
2021-06-16 22:56:05 -0300

    Implementação do metodo listarPropostasCliente()

--- src/negocio/ControladorProposta.java
+++ src/negocio/ControladorProposta.java
@@ -2,11 +2,17 @@ package negocio;
 
 import dados.Repositorio;
 import dados.RepositorioCRUD;
+import exceptions.ObjetoDuplicadoException;
+import jdk.vm.ci.meta.Local;
 import negocio.beans.Cliente;
 import negocio.beans.Proposta;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class ControladorProposta {
     private Repositorio<Proposta> repoProposta;
@@ -15,12 +21,12 @@ public class ControladorProposta {
         this.repoProposta = new RepositorioCRUD<>();
     }
 
-    public void criarProposta() {
-
-    }
-
-    public void criarContraProposta(Proposta proposta) {
-
+    public void criarProposta(Proposta p) throws ObjetoDuplicadoException {
+        try {
+            this.repoProposta.inserir(p);
+        } catch (ObjetoDuplicadoException e) {
+            throw new ObjetoDuplicadoException(e);
+        }
     }
 
     public String propostaEmDetalhe() {
@@ -29,10 +35,16 @@ public class ControladorProposta {
         return proposta.toString();
     }
 
-    public List<Proposta> listarPropostasCliente(long uidCliente) {
-        List<Proposta> propostasClientes = new ArrayList<>();
-
-        return propostasClientes;
+    public Map<LocalDate, Proposta> listarPropostasCliente(long uidCliente) {
+        NavigableMap<LocalDate, Proposta> mapaPropostas = new TreeMap<>();
+        
+        for (Proposta proposta : this.repoProposta.listar()) {
+            if(proposta.getCliente().getUid() == uidCliente){
+                //Preencher mapa
+                mapaPropostas.put(proposta.getData(), proposta);
+            }
+        }
+        return mapaPropostas;
     }
 
     public List<Proposta> listarContraPropostas(long uidCliente) {
