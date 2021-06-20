97287f29fd0f479148e2cd113879082881bc0436
gerorsch <62017578+gerorsch@users.noreply.github.com>
2021-06-19 22:58:01 -0300

    Adiciona método inserirBens()
    Método que insere bem no repositório de bens. O objeto receberá a data do cadastro e receberá os parâmetros
    falso para o atributo {@code garantia} e verdadeiro para o atributo {@code pendente}, e a exceção BensDuplicadoException caso o bem cadastrado já tenha sido inserido.

--- src/negocio/ControladorBENS.java
+++ src/negocio/ControladorBENS.java
@@ -17,6 +17,23 @@ public class ControladorBENS {
 
     public void inserirBens(Bens bens) {
 
+    /**
+     * Método que insere bem no repositório.
+     * @param bens bem a ser inserido no repositório. O objeto receberá a data do cadastro e receberá os parametros
+     *             falso para o atributo {@code garantia} e verdadeiro para o atributo {@code pendente}.
+     * @throws BensDuplicadoException caso o bem cadastrado já tenha sido inserido.
+     */
+    public void inserirBens(Bens bens) throws BensDuplicadoException{
+       //Atribui a data de cadastro do bem
+        bens.setDataCadastro(LocalDate.now());
+        //Seta os valores para os atributos pendente e garantia
+        bens.setPendente(true);
+        bens.setGarantia(false);
+
+        try {
+            this.repoBENS.inserir(bens);
+        } catch (ObjetoDuplicadoException e) {
+            throw new BensDuplicadoException("Bens já registrado no sistema!");
     }
 
     public List<Bens> listarBensEmpresa() {
