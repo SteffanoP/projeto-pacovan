3efc25be4e8a2740e141315aa493d38483dec7e6
dnlbyte <75702871+dnlbyte@users.noreply.github.com>
2021-06-19 23:36:19 -0300

    Métodos de listagem de movimentação e bens (#18)
    * Update Bens.java
    
    * Update ControladorMovimentacao.java
    
    * Update ControladorBENS.java
    
    * Update Bens.java
    
    * Update ControladorBENS.java
    
    * Update ControladorMovimentacao.java
    
    * Update ControladorMovimentacao.java
    
    * Update ControladorBENS.java
    
    * Update ControladorMovimentacao.java
    
    Co-authored-by: dnlbyte <daniel_lma@hotmail.com>

--- src/negocio/ControladorBENS.java
+++ src/negocio/ControladorBENS.java
@@ -2,11 +2,13 @@ package negocio;
 
 import dados.Repositorio;
 import dados.RepositorioCRUD;
+import exceptions.PessoaInexistenteException;
 import negocio.beans.Bens;
 import negocio.beans.Cliente;
+import negocio.beans.Movimentacao;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
 
 public class ControladorBENS {
     private Repositorio<Bens> repoBENS;
@@ -19,38 +21,92 @@ public class ControladorBENS {
 
     }
 
-    public List<Bens> listarBensEmpresa() {
-        List<Bens> bensList = new ArrayList<>();
-
-        return bensList;
+    public Map<LocalDate,Bens> listarBensEmpresa() {
+        NavigableMap<LocalDate, Bens> mapaBens = new TreeMap<>();
+        List<Bens> benEmpresaList = repoBENS.listar();
+            for( Bens ben : benEmpresaList){
+               mapaBens.put(ben.getDataCadastro(), ben);
+            }
+        return mapaBens;
     }
 
-    public List<Bens> listarBensCliente(long uidCliente) {
-        List<Bens> bensList = new ArrayList<>();
+    public Map<LocalDate,Bens> listarBensCliente(long uidCliente) throws PessoaInexistenteException {
+        NavigableMap<LocalDate, Bens> mapaBensCliente = new TreeMap<>();
+        boolean benClienteExiste = false;
+        List<Bens> benClienteList = repoBENS.listar();
 
-        return bensList;
+        for(Bens ben : benClienteList){
+            if(ben.getCliente().getUid() == uidCliente) {
+
+                benClienteExiste = true;
+                mapaBensCliente.put(ben.getDataCadastro(), ben);
+            }
+
+        }if(!benClienteExiste)  throw new PessoaInexistenteException("Cliente Não existe!");
+            return mapaBensCliente;
+}
+
+
+    public  Map<LocalDate,Bens> listarBensPendentes(long uidCliente) throws PessoaInexistenteException{
+
+        NavigableMap<LocalDate, Bens> mapaBensPendentes = new TreeMap<>();
+        boolean pendente = false;
+        List<Bens> pendenteList = repoBENS.listar();
+
+        for(Bens ben : pendenteList){
+            if(ben.getCliente().getUid() == uidCliente && ben.isPendente()) {
+
+                pendente = true;
+
+                mapaBensPendentes.put(ben.getDataCadastro(), ben);
+            }
+        }
+        if(!pendente)  throw new PessoaInexistenteException("Cliente Não existe!");
+
+        return mapaBensPendentes;
     }
 
-    public List<Bens> listarBensPendentes(long uidCliente) {
-        List<Bens> bensList = new ArrayList<>();
+    public  Map<LocalDate,Bens> listarBensAprovados(long uidCliente) throws PessoaInexistenteException{
+        NavigableMap<LocalDate, Bens> mapaBensaprovados = new TreeMap<>();
+        boolean aprovado = false;
+        List<Bens> aproveList = repoBENS.listar();
 
-        return bensList;
+        for(Bens ben : aproveList){
+            if(ben.getCliente().getUid() == uidCliente && (!ben.isPendente())) {
+              aprovado = true;
+
+              mapaBensaprovados.put(ben.getDataCadastro(), ben);
+            }
+        }if(!aprovado)  throw new PessoaInexistenteException("Cliente Não existe!");
+
+        return mapaBensaprovados;
     }
 
-    public List<Bens> listarBensAprovados(long uidCliente) {
-        List<Bens> bensList = new ArrayList<>();
+    public Map<LocalDate,Bens> listarBensGarantia (long uidCliente) throws PessoaInexistenteException{
+        NavigableMap<LocalDate, Bens> mapaBensGarantia = new TreeMap<>();
+        boolean garantia = false;
+        List<Bens> garantiaList = repoBENS.listar();
 
-        return bensList;
+            for(Bens ben: garantiaList){
+                if(ben.getCliente().getUid() == uidCliente && ben.isGarantia()){
+                    garantia = true;
+                    mapaBensGarantia.put(ben.getDataCadastro(), ben);
+                }
+            }if(!garantia)  throw new PessoaInexistenteException ("Cliente Não existe!");
+
+        return mapaBensGarantia;
     }
 
-    public List<Bens> listarBensEmGarantia(long uidCliente) {
-        List<Bens> bensList = new ArrayList<>();
-
-        return bensList;
-    }
-
-    public double calcularValorBensCliente(long uidCliente) {
+    public double calcularValorBensCliente(long uidCliente) throws PessoaInexistenteException{
         double valor = 0;
+        boolean existevalor  = false;
+        List<Bens> valorBenList = repoBENS.listar();
+        for(Bens ben: valorBenList) {
+            if (ben.getCliente().getUid() == uidCliente){
+                existevalor = true;
+                valor = ben.getValor() ;
+            }
+        }if(!existevalor)  throw new PessoaInexistenteException("Cliente Não existe!");
 
         return valor;
     }
--- src/negocio/ControladorMovimentacao.java
+++ src/negocio/ControladorMovimentacao.java
@@ -2,11 +2,12 @@ package negocio;
 
 import dados.Repositorio;
 import dados.RepositorioCRUD;
+import exceptions.PessoaInexistenteException;
+import negocio.beans.Bens;
 import negocio.beans.Movimentacao;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ControladorMovimentacao {
     private Repositorio<Movimentacao> repoMovimentacao;
@@ -15,10 +16,21 @@ public class ControladorMovimentacao {
         this.repoMovimentacao = new RepositorioCRUD<>();
     }
 
-    public List<Movimentacao> listarTodasMovimentacaoCliente(long uidCliente) {
-        List<Movimentacao> movimentacaoList = new ArrayList<>();
+    public Map<LocalDateTime, Movimentacao> listarMoveCliente(long uidCliente) throws PessoaInexistenteException  {
+        NavigableMap<LocalDate, Movimentacao> mapaMovimentacaoCliente = new TreeMap<>();
+        boolean moveClienteExiste = false;
+        List<Movimentacao> moveList = repoMovimentacao.listar();
 
-        return movimentacaoList;
+        for( Movimentacao move : moveList){
+
+            if(move.getCliente().getUid() == uidCliente ){
+                moveClienteExiste = true;
+                    mapaMovimentacaoCliente.put(move.getInstante(), move);  //duvida
+            }
+        }if(!moveClienteExiste)  throw new PessoaInexistenteException ("Cliente Não existe!");
+
+
+        return mapaMovimentacaoCliente;
     }
 
     public List<Movimentacao> listarPeriodoMovimentacaoCliente(long uidCliente, LocalDate dataInicial, LocalDate dataFinal) {
