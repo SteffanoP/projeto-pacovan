5603d54443f25360084391bb3b25134b09d43ff5
SteffanoP <steffanoxpereira@gmail.com>
2021-06-21 16:02:20 -0300

    feat: tela inicial (primary stage)

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sistema de Gerenciamento de Empréstimo Pessoal Alternativo");
+
+        primaryStage.setWidth(1024);
+        primaryStage.setHeight(720);
+
+        primaryStage.show();
+    }
+}
