2b05fe9b4c9915d61b2d6e55b00486f30cca9a09
SteffanoP <steffanoxpereira@gmail.com>
2021-06-28 17:38:37 -0300

    feat: apresentação do empregado na tela principal

--- src/gui/TelaPrincipalEmpregadoController.java
+++ src/gui/TelaPrincipalEmpregadoController.java
@@ -1,13 +1,29 @@
 package gui;
 
+import gerenciamento.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import negocio.Fachada;
 
 public class TelaPrincipalEmpregadoController {
+    @FXML Label lblNomeUsuario;
+    @FXML Button btnMeusDados;
 
     @FXML Button btnAnaliseProposta;
     @FXML Button btnConfirmarDevedor;
+
+    private boolean initialized = false;
+
+    private void initialize() {
+        lblNomeUsuario.setText(SessionManager.getInstance().getPessoaSessao().getNome());
+    }
+
+    @FXML
+    public void btnMeusDadosPressed() {
+        GerenciadorTelas.getInstance().changeScreen("telaInformacoesPessoais");
+    }
 
     public void btnAnalisePropostaPressed(ActionEvent event) {
         GerenciadorTelas.getInstance().changeScreen("telaAnaliseProposta");
@@ -19,5 +35,13 @@ public class TelaPrincipalEmpregadoController {
 
     public void btnVoltarPressed(ActionEvent event) {
         GerenciadorTelas.getInstance().changeScreen("telaLogin");
+    }
+
+    @FXML
+    public void onMouseEntered() {
+        if (!initialized) {
+            this.initialize();
+            initialized = true;
+        }
     }
 }
