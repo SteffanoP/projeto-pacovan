8874565a1db54444895806c75b018588e48c1b62
Fedra Brito <61056733+hagafedra@users.noreply.github.com>
2021-07-09 15:11:28 -0300

    Segunda versão da Tela Principal do Admin (#112)
    * Tela Admin versão 2
    
    * fix: Erro fxml
    
    * fix: método inicializeTableViews()
    
    * fix: inicialização das tableviews de devedor
    
    Co-authored-by: SteffanoP <steffanoxpereira@gmail.com>

--- src/gui/TelaPrincipalAdminController.java
+++ src/gui/TelaPrincipalAdminController.java
@@ -1,27 +1,52 @@
 package gui;
 
+import gui.models.DevedorModelo;
 import gui.models.EmpregadoModelo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import negocio.Fachada;
-import negocio.beans.Cliente;
 import negocio.beans.Empregado;
 import java.util.List;
 
 import exceptions.PessoaInexistenteException;
 import gerenciamento.SessionManager;
+import negocio.beans.Emprestimo;
 import negocio.beans.Pessoa;
 
 public class TelaPrincipalAdminController {
+	@FXML Label lblNomeUsuario;
+    
     @FXML TableView<EmpregadoModelo> tblvEmpregados;
     @FXML TableColumn<EmpregadoModelo, String> colNomeEmpregados;
     @FXML TableColumn<EmpregadoModelo, Float> colReputacaoEmpregados;
+    
+    @FXML TableView<DevedorModelo> tblvDevedores;
+    @FXML TableColumn<DevedorModelo, Double> colValorDevidoDevedores;
+    @FXML TableColumn<DevedorModelo, String> colDataPagamentoDevedores;
+    @FXML TableColumn<DevedorModelo, String> colNomeClienteDevedores;
+    @FXML TableColumn<DevedorModelo, Double> colParcelasDevedores;
+    @FXML TableColumn<DevedorModelo, Float> colConfiancaPagamentoDevedores;
+
+    @FXML TableView<DevedorModelo> tblvDProtegidos;
+    @FXML TableColumn<DevedorModelo, Double> colValorDProtegidos;
+    @FXML TableColumn<DevedorModelo, String> colDataPagamentoDProtegidos;
+    @FXML TableColumn<DevedorModelo, String> colNomeClienteDProtegidos;
+    @FXML TableColumn<DevedorModelo, Double> colParcelasDProtegidos;
+    @FXML TableColumn<DevedorModelo, Float> colConfiancaPagamentoProtegidos;
+
+    @FXML TableView<DevedorModelo> tblvDAltoRisco;
+    @FXML TableColumn<DevedorModelo, Double> colValorDevidoDAltoRisco;
+    @FXML TableColumn<DevedorModelo, String> colDataPagamentoDAltoRisco;
+    @FXML TableColumn<DevedorModelo, String> colNomeClienteDAltoRisco;
+    @FXML TableColumn<DevedorModelo, Double> colParcelasDAltoRisco;
+    @FXML TableColumn<DevedorModelo, Float> colConfiancaPagamentoAltoRisco;
 
     @FXML Button btnGerarRelatorio;
     @FXML Button btnCadastrarEmpregado;
@@ -29,11 +54,16 @@ public class TelaPrincipalAdminController {
 
     @FXML
     private void initialize() {
-        this.initializeTableView();
-        this.atualizarTableView(tblvEmpregados, Fachada.getInstance().listarEmpregados());
+    	lblNomeUsuario.setText(SessionManager.getInstance().getPessoaSessao().getNome());
+    	
+        this.initializeTableViews();
+        this.atualizarTableViewEmpregado(tblvEmpregados, Fachada.getInstance().listarEmpregados());
+        this.atualizarTableViewDevedor(tblvDevedores, Fachada.getInstance().listarDevedores());
+        this.atualizarTableViewDevedor(tblvDProtegidos, Fachada.getInstance().listarDevedoresProtegidos());
+        this.atualizarTableViewDevedor(tblvDAltoRisco, Fachada.getInstance().listarDevedoresAltoRisco());
     }
 
-    private void atualizarTableView(TableView<EmpregadoModelo> tableView, List<Empregado> empregadoList) {
+    private void atualizarTableViewEmpregado(TableView<EmpregadoModelo> tableView, List<Empregado> empregadoList) {
         for (Empregado empregado : empregadoList) {
             EmpregadoModelo empregadoModelo = new EmpregadoModelo(empregado.getNome(), empregado.getReputacao(),
                     empregado.getEmail());
@@ -41,10 +71,13 @@ public class TelaPrincipalAdminController {
         }
     }
 
-    private void initializeTableView() {
-        //Initialize tableview
-        colNomeEmpregados.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colReputacaoEmpregados.setCellValueFactory(new PropertyValueFactory<>("reputacao"));
+    private void atualizarTableViewDevedor(TableView<DevedorModelo> tableView, List<Emprestimo> emprestimoList) {
+        for (Emprestimo emprestimo : emprestimoList) {
+            DevedorModelo devedorModelo = new DevedorModelo(emprestimo.getValor(), emprestimo.getDataPagamento(),
+                    emprestimo.getCliente(),emprestimo.getParcelas(),emprestimo.getConfiancaPagamento(),
+                    emprestimo.getCliente().getEmail());
+            tableView.getItems().add(devedorModelo);
+        }
     }
 
     public void btnGerarRelatorioPressed(ActionEvent event) {
@@ -78,6 +111,31 @@ public class TelaPrincipalAdminController {
 				e.printStackTrace();
 			}
     	}
+    }
+    
+    private void initializeTableViews() {
+    	//Initialize tableview Empregados
+    	colNomeEmpregados.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colReputacaoEmpregados.setCellValueFactory(new PropertyValueFactory<>("reputacao"));
+        //Seta as properties da TableView Devedores
+        this.setCellValuesPropertiesDevedores(colValorDevidoDevedores, colDataPagamentoDevedores, colNomeClienteDevedores,
+                colParcelasDevedores,colConfiancaPagamentoDevedores);
+
+        //Seta as properties da TableView Devedores Protegidos
+        this.setCellValuesPropertiesDevedores(colValorDProtegidos, colDataPagamentoDProtegidos, colNomeClienteDProtegidos,
+                colParcelasDProtegidos, colConfiancaPagamentoProtegidos);
+
+        //Seta as properties da Tableview Devedores Alto Risco
+        this.setCellValuesPropertiesDevedores(colValorDevidoDAltoRisco, colDataPagamentoDAltoRisco, colNomeClienteDAltoRisco,
+                colParcelasDAltoRisco, colConfiancaPagamentoAltoRisco);
+    }
+
+    private void setCellValuesPropertiesDevedores(TableColumn<DevedorModelo, Double> colValorDevido, TableColumn<DevedorModelo, String> colDataPagamento, TableColumn<DevedorModelo, String> colNomeCliente, TableColumn<DevedorModelo, Double> colParcelas, TableColumn<DevedorModelo, Float> colConfiancaPagamento) {
+        colValorDevido.setCellValueFactory(new PropertyValueFactory<>("valorDevido"));
+        colDataPagamento.setCellValueFactory(new PropertyValueFactory<>("dataPagamento"));
+        colNomeCliente.setCellValueFactory(new PropertyValueFactory<>("nomeCliente"));
+        colParcelas.setCellValueFactory(new PropertyValueFactory<>("parcelas"));
+        colConfiancaPagamento.setCellValueFactory(new PropertyValueFactory<>("confiancaPagamento"));
     }
     
     @FXML
